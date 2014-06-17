a04ba5e7c9658a851baf0b92f45aa7bd0e5440eb
Simon Garcia <simon.garciar@gmail.com>
2014-06-17 20:57:28 +0200

    add after_signup_path for the devise redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path(resource)
     dashboard_path
   end
+
+  def after_sign_up_path(resource)
+    dashboard_path
+  end
 end
