0136a47be21b18baa61c7cea2d76f952fd0bfb79
Simon Garcia <simon.garciar@gmail.com>
2014-06-19 18:06:19 +0200

    remove #after_sign[up|in]_path methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  protected
-
-  def after_sign_in_path(resource)
-    dashboard_path
-  end
-
-  def after_sign_up_path(resource)
-    dashboard_path
-  end
 end
