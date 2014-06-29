aa8039b424661634f1fc2b9c423e56eed0723194
Simon Garcia <simon.garciar@gmail.com>
2014-06-29 18:47:45 +0200

    remove unused spec on dashboard controller

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, :type => :controller do
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      expect(response).to be_success
-    end
-  end
-
 end
