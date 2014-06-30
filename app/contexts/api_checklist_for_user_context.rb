59bf89b6e8315f6a415c032fe1ef4d44ab19c492
Simon Garcia <simon.garciar@gmail.com>
2014-07-01 01:41:25 +0200

    s/survey/checklist on checklist_for_user_context

--- app/contexts/api_checklist_for_user_context.rb
+++ app/contexts/api_checklist_for_user_context.rb
@@ -1,12 +1,12 @@
 class ApiChecklistForUserContext < ApiBaseContext
-  def initialize(user_id, survey_id)
+  def initialize(user_id, checklist_id)
     super(user_id)
-    @survey_id = survey_id
+    @checklist_id = checklist_id
   end
 
   def execute
     Checklist.
-      where(:id => @survey_id, user_id: @user_id).
+      where(:id => @checklist_id, user_id: @user_id).
       select(:id, :name, :description).
       includes(:check_items).
       first    
