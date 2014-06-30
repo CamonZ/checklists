36f7763f24f2df462de9881061a55c232ecf9484
Simon Garcia <simon.garciar@gmail.com>
2014-07-01 00:09:26 +0200

    work on edit portion of new checklist template, add show template for checklsits

---
+++ app/contexts/creating_a_survey_for_user_context.rb
@@ -0,0 +1,21 @@
+class CreatingASurveyForUserContext < ApiBaseContext
+  def initialize(user_id, params)
+    super(user_id)
+    @params = params
+  end
+
+  def execute
+    survey = Survey.new(params_for_survey)
+    survey.save
+    survey
+  end
+
+  private
+
+  def params_for_survey
+    @params.
+      require(:survey).
+      permit(:checklist_id, check_item_results_attributes: [:check_item_id, :value]).
+      merge({user_id: @user_id})
+  end
+end
